b1b516366a80881a64f15e3ab67859978dd4b414
Mike Ling <77037526+m3t4m1k3@users.noreply.github.com>
2022-06-09 13:13:04 +0000

    Add template_literals.js

---
+++ template_literals.js
@@ -0,0 +1,21 @@
+// Template Literals
+
+// Use back ticks
+``;
+
+// Equivalent to string
+console.log(`String` === "String");
+// true
+
+// Interpolating with ${} converts the value to a string by calling the .toString() method
+// Does not work well for arrays and objects
+
+console.log([1, 2, 3].toString());
+// '1,2,3'
+
+console.log({ a: 1 }.toString());
+// [object Object]
+
+const obj = {a: 1};
+console.log(`an object: ${obj}`)
+// "an object: [object Object]
